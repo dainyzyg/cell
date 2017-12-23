5a32a6f690bc05e3c4205abcae8c4a27b840f6a3
YiDao <YiDao@YiDaodeMacBook-Pro.local>
2017-12-23 13:51:52 +0800

    up

--- vue/view/home.vue
+++ vue/view/home.vue
@@ -1,33 +1,231 @@
+<style scoped>
+.left {
+    background-color: rgb(42, 63, 84);
+    flex: 0 0 240px;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+}
+
+.right {
+    background-color: rgb(247, 247, 247);
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+}
+
+.title {
+    flex: 0 0 55px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    padding-left: 20px;
+    color: whitesmoke;
+}
+
+.menu {
+    flex: 1;
+    overflow-y: auto;
+}
+
+.ivu-menu {
+    background-color: rgb(42, 63, 84);
+    color: rgba(255, 255, 255, .75) !important;
+    font-weight: bold;
+}
+
+.page>>>.ivu-menu-opened .ivu-menu-submenu-title {
+    color: white;
+    text-shadow: rgba(0, 0, 0, .25) 0 -1px 0 !important;
+    background: linear-gradient(#334556, #2C4257), #2A3F54 !important;
+    box-shadow: rgba(0, 0, 0, .25) 0 1px 0, inset rgba(255, 255, 255, .16) 0 1px 0 !important;
+}
+
+.page>>>.ivu-menu-submenu-title:hover {
+    background-color: rgb(42, 63, 84);
+    color: white;
+}
+
+.ivu-menu-item {
+    padding-left: 50px !important;
+    color: rgba(255, 255, 255, .75);
+    font-weight: normal;
+}
+
+.ivu-menu-item:before {
+    background: #425668;
+    bottom: auto;
+    content: "";
+    height: 8px;
+    left: 26px;
+    top: 20px;
+    position: absolute;
+    right: auto;
+    width: 8px;
+    z-index: 1;
+    border-radius: 50%;
+}
+
+.ivu-menu-item:after {
+    border-left: 1px solid #425668;
+    bottom: 0;
+    content: "";
+    left: 30px;
+    position: absolute;
+    top: 0;
+}
+
+.ivu-menu-item-active.ivu-menu-item-selected {
+    /*background-color: rgba(255, 255, 255, 0.05) !important;*/
+    color: white !important;
+    border-right: 5px solid #1ABB9C !important;
+}
+
+.ivu-menu-item:hover {
+    background-color: rgba(255, 255, 255, 0.05) !important;
+}
+
+.ivu-menu-vertical.ivu-menu-light:after {
+    width: 0;
+}
+
+.topbar {
+    background: #EDEDED;
+    border-bottom: 1px solid #D9DEE4;
+    flex: 0 0 58px;
+    display: flex;
+}
+
+.topbar-left {
+    flex: 1;
+    padding-left: 20px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.topbar-right {
+    flex: 1;
+    padding-right: 20px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.topbar-drop {
+    cursor: pointer;
+    color: #73879c;
+    font-size: 14px;
+    font-weight: bold;
+    display: flex;
+    justify-content: space-between;
+    width: 120px;
+    align-items: center;
+}
+
+.container {
+    width: 500px;
+    padding: 20px;
+}
+</style>
 <template>
-    <div>
-        welcom home
+    <div class="page">
+        <div class="left">
+            <div class="title">
+                <Icon type="ionic" style="margin-right:15px;" size=26></Icon>
+                <h2>管理平台</h2>
+            </div>
+            <div class="menu">
+                <Menu :open-names="['1']" accordion>
+                    <Submenu name="1">
+                        <template slot="title">
+                            <Icon type="ios-paper"></Icon>
+                            内容管理
+                        </template>
+                        <Menu-item name="1-1">文章管理</Menu-item>
+                        <Menu-item name="1-2">评论管理</Menu-item>
+                        <Menu-item name="1-3">举报管理</Menu-item>
+                    </Submenu>
+                    <Submenu name="2">
+                        <template slot="title">
+                            <Icon type="ios-people"></Icon>
+                            用户管理
+                        </template>
+                        <Menu-item name="2-1">新增用户</Menu-item>
+                        <Menu-item name="2-2">活跃用户</Menu-item>
+                    </Submenu>
+                    <Submenu name="3">
+                        <template slot="title">
+                            <Icon type="stats-bars"></Icon>
+                            统计分析
+                        </template>
+                        <Menu-group title="使用">
+                            <Menu-item name="3-1">新增和启动</Menu-item>
+                            <Menu-item name="3-2">活跃分析</Menu-item>
+                            <Menu-item name="3-3">时段分析</Menu-item>
+                        </Menu-group>
+                        <Menu-group title="留存">
+                            <Menu-item name="3-4">用户留存</Menu-item>
+                            <Menu-item name="3-5">流失用户</Menu-item>
+                        </Menu-group>
+                    </Submenu>
+                </Menu>
+            </div>
+        </div>
+        <div class="right">
+            <div class="topbar">
+                <div class="topbar-left">
+                    <Icon type="navicon-round" size=26></Icon>
+                </div>
+                <div class="topbar-right">
+                    <Dropdown trigger="click" placement="bottom-end">
+                        <div class="topbar-drop">
+                            <Icon type="person" size=21></Icon>
+                            <span>系统管理员</span>
+                            <Icon type="arrow-down-b"></Icon>
+                        </div>
+                        <Dropdown-menu slot="list">
+                            <Dropdown-item>个人资料</Dropdown-item>
+                            <Dropdown-item>设置</Dropdown-item>
+                            <Dropdown-item>登出</Dropdown-item>
+                        </Dropdown-menu>
+                    </Dropdown>
+                </div>
+            </div>
+            <div class="container">
+                <formc></formc>
+            </div>
+        </div>
     </div>
 </template>
 <script>
-    export default {
-        data() {
-            return {
-                text: 'ewdsfsss'
+import formc from '../compoments/form.vue'
+export default {
+    components: {
+        formc
+    },
+    data() {
+        return {
+            spanLeft: 5,
+            spanRight: 19
+        }
+    },
+    computed: {
+        iconSize() {
+            return this.spanLeft === 5 ? 14 : 24;
+        }
+    },
+    methods: {
+        toggleClick() {
+            if (this.spanLeft === 5) {
+                this.spanLeft = 2;
+                this.spanRight = 22;
+            } else {
+                this.spanLeft = 5;
+                this.spanRight = 19;
             }
         }
-
     }
+}
 </script>
-<style scoped>
-    .loading {
-        color: white;
-        position: fixed;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        justify-content: center;
-        align-items: center;
-        z-index: 1;
-        background-color: rgba(0, 0, 0, .2);
-    }
-
-    .mui-spinner {
-        font-size: 30px;
-    }
-</style>
--- vue/view/login.vue
+++ vue/view/login.vue
@@ -1,6 +1,23 @@
 <template>
-    <div class="page">
+    <div class="page wrapper">
+        <ul class="bg-bubbles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
         <Form class="form" ref="formInline" :model="formInline" :rules="ruleInline">
+            <div class="title">
+                <Icon type="ionic" style="margin-right:15px;" size=40></Icon>
+                <h1>管理平台</h1>
+            </div>
             <Form-item prop="user">
                 <Input type="text" size="large" v-model="formInline.user" placeholder="Username">
                 <Icon style="width:15px;" type="ios-person-outline" slot="prepend"></Icon>
@@ -12,7 +29,7 @@
                 </Input>
             </Form-item>
             <Form-item>
-                <Button long size="large" type="primary" @click="handleSubmit('formInline')">登录</Button>
+                <Button long size="large" class="btn" @click="handleSubmit('formInline')">登录</Button>
             </Form-item>
         </Form>
     </div>
@@ -48,29 +65,177 @@
         },
         methods: {
             handleSubmit(name) {
+                console.log('handleSubmit')
                 this.$store.commit('login')
-                this.$refs[name].validate((valid) => {
-                    if (valid) {
-                        this.$Message.success('提交成功!');
-                    } else {
-                        this.$Message.error('表单验证失败!');
-                    }
-                })
+                // this.$refs[name].validate((valid) => {
+                //     if (valid) {
+                //         this.$Message.success('提交成功!');
+                //     } else {
+                //         this.$Message.error('表单验证失败!');
+                //     }
+                // })
             }
         }
     }
 </script>
-<style>
+<style scoped>
     .page {
-        background-color: #2d3a4b;
-        flex: 1;
+        background-color: rgb(42, 63, 84);
         display: flex;
         align-items: center;
-        align-self: stretch;
         justify-content: center;
     }
 
     .form {
         width: 300px;
+        z-index: 100;
+    }
+
+    .title {
+        height: 80px;
+        display: flex;
+        color: white;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 24px;
+        border-bottom: 2px solid rgba(255, 255, 255, 0.2);
+    }
+
+    .wrapper {
+        /*background: #50a3a2;*/
+        /*background:linear-gradient(-225deg, #7085B6 0%, #87A7D9 50%, #DEF3F8 100%);*/
+        /*background: linear-gradient(to bottom right, #2d3a4b 0%, #2d8cf0 100%);*/
+        /*background: linear-gradient(to right bottom, #243949 0%, #517fa4 100%);*/
+        opacity: 0.9;
+    }
+
+    .btn {
+        background-color: #286090;
+        color: white;
+        font-weight: bold;
+        border-color: #286090;
+    }
+
+    .bg-bubbles {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 1;
+    }
+
+    .bg-bubbles li {
+        position: absolute;
+        list-style: none;
+        display: block;
+        width: 40px;
+        height: 40px;
+        background-color: rgba(255, 255, 255, 0.15);
+        bottom: -160px;
+        -webkit-animation: square 25s infinite;
+        animation: square 15s infinite;
+        -webkit-transition-timing-function: linear;
+        transition-timing-function: linear;
+    }
+
+    .bg-bubbles li:nth-child(1) {
+        left: 10%;
+    }
+
+    .bg-bubbles li:nth-child(2) {
+        left: 20%;
+        width: 80px;
+        height: 80px;
+        -webkit-animation-delay: 2s;
+        animation-delay: 2s;
+        -webkit-animation-duration: 17s;
+        animation-duration: 17s;
+    }
+
+    .bg-bubbles li:nth-child(3) {
+        left: 25%;
+        -webkit-animation-delay: 4s;
+        animation-delay: 4s;
+    }
+
+    .bg-bubbles li:nth-child(4) {
+        left: 40%;
+        width: 60px;
+        height: 60px;
+        -webkit-animation-duration: 22s;
+        animation-duration: 22s;
+        background-color: rgba(255, 255, 255, 0.25);
+    }
+
+    .bg-bubbles li:nth-child(5) {
+        left: 70%;
+    }
+
+    .bg-bubbles li:nth-child(6) {
+        left: 80%;
+        width: 120px;
+        height: 120px;
+        -webkit-animation-delay: 3s;
+        animation-delay: 3s;
+        background-color: rgba(255, 255, 255, 0.2);
+    }
+
+    .bg-bubbles li:nth-child(7) {
+        left: 32%;
+        width: 160px;
+        height: 160px;
+        -webkit-animation-delay: 7s;
+        animation-delay: 7s;
+    }
+
+    .bg-bubbles li:nth-child(8) {
+        left: 55%;
+        width: 20px;
+        height: 20px;
+        -webkit-animation-delay: 15s;
+        animation-delay: 15s;
+        -webkit-animation-duration: 40s;
+        animation-duration: 40s;
+    }
+
+    .bg-bubbles li:nth-child(9) {
+        left: 25%;
+        width: 10px;
+        height: 10px;
+        -webkit-animation-delay: 2s;
+        animation-delay: 2s;
+        -webkit-animation-duration: 40s;
+        animation-duration: 40s;
+        background-color: rgba(255, 255, 255, 0.3);
+    }
+
+    .bg-bubbles li:nth-child(10) {
+        left: 90%;
+        width: 160px;
+        height: 160px;
+        -webkit-animation-delay: 11s;
+        animation-delay: 11s;
+    }
+    /*@-webkit-keyframes square {
+        0% {
+            -webkit-transform: translateY(0);
+            transform: translateY(0);
+        }
+        100% {
+            -webkit-transform: translateY(-700px) rotate(600deg);
+            transform: translateY(-700px) rotate(600deg);
+        }
+    }*/
+
+    @keyframes square {
+        0% {
+            /*-webkit-transform: translateY(0);*/
+            transform: translateY(0);
+        }
+        100% {
+            /*-webkit-transform: translateY(-700px) rotate(600deg);*/
+            transform: translateY(calc(-100vh - 200px)) rotate(600deg);
+        }
     }
 </style>
